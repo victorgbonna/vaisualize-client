130d3fca64c6a15d38dcf5aea31f2a5aecac5c60
victorgbonna <victorgbonna@gmail.com>
2025-11-18 10:33:44 +0100

    connected to render

--- src/configs/baseUrl/index.js
+++ src/configs/baseUrl/index.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://football-cleansheet-data-api.onrender.com"
+    ? "https://vaisualize-api.onrender.com"
       // : "http://localhost:5002";
     // :"http://192.168.153.36:5002";
     : "http://localhost:5001";
