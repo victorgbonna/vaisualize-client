723b36284c2a6924194d8c48de6a7f9d1bfed72a
victorgbonna <victorgbonna@gmail.com>
2026-08-04 04:37:00 +0100

    tweak response to suite  data structure

--- src/pages/app/projects/add-chart/[id].jsx
+++ src/pages/app/projects/add-chart/[id].jsx
@@ -4,7 +4,7 @@ import { useHttpServices } from "@/hooks"
 import { useQuery } from "@tanstack/react-query"
 import { useRouter } from "next/router"
 import { useMemo } from "react"
-
+// dd
 export default function AddChart(){
     const router=useRouter()    
 
